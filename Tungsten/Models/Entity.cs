d17a0b68ae0fc045dc572e6fc1d351308a844f7b
Emil Eklund <emileklund@outlook.com>
2016-12-20 11:46:42 +0100

    Adding Core Models.

---
+++ Tungsten/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tungsten.Models
+{
+    public interface Entity<TKey>
+    {
+         TKey Id { get; set; }
+    }
+}
