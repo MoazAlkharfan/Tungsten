1fb2c992e4f82be38d9b1bba5b672820cd07a1e0
MZDisaster <moaazw@gmail.com>
2016-12-20 12:00:08 +0100

    getset error

--- Tungsten/DataAccessLayer/SchoolContext.cs
+++ Tungsten/DataAccessLayer/SchoolContext.cs
@@ -9,11 +9,11 @@ namespace Tungsten.DataAccessLayer
 {
     public class SchoolContext : DbContext, ISchoolContext
     {
-        public DbSet<Group> Groups;
-        public DbSet<Course> Courses;
-        public DbSet<Segment> Segments;
-        public DbSet<Assignment> Assignments;
-        public DbSet<Lesson> Lessons;
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Segment> Segments { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
 
         public SchoolContext() :base("DefaultConnection")
         {
