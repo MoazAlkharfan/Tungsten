a187054115b086f5ddbee29fca2d3a2267556a84
KlasClaywood <klas.claywood@gmail.com>
2016-12-21 10:59:27 +0100

    UnitTests group

---
+++ Tungsten.Tests/GroupUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tungsten.Models;
+
+namespace Tungsten.Tests
+{
+    [TestClass]
+    public class GroupUnitTest
+    {
+        [TestMethod]
+        public void NewGroupGetsGUID_Id()
+        {
+            Group grupp = new Group();
+
+        }
+    }
+}
