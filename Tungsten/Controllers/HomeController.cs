f7f76d58d0ed00a4e82df8872e9344437b5fbd6a
MZDisaster <moaazw@gmail.com>
2016-12-21 08:55:28 +0100

    Extended AuthorizeAttribute to not show login when requesting un authorized action based on role when already logged in.

--- Tungsten/Controllers/HomeController.cs
+++ Tungsten/Controllers/HomeController.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Tungsten.Models;
+using Tungsten.Repositories;
+using Tungsten.Attributes;
 
 namespace Tungsten.Controllers
 {
+    [ExtendedAuthorize]
     public class HomeController : Controller
     {
+        private ISchoolRepo repo;
+
+        public HomeController()
+        {
+            this.repo = new SchoolRepo();
+        }
+
+        public HomeController(ISchoolRepo newrepo)
+        {
+            this.repo = newrepo;
+        }
+
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("PanelIndex");
+
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public ActionResult PanelIndex()
+        {
+            return View();
+        }
+
+        public JsonResult CreateCourse(Course course)
+        {
+            if(ModelState.IsValid)
+            {
+                repo.CreateCourse(course);
+                return Json(new { status = "Success" });
+            }
+            else
+            {
+                return Json(new { status = "Error"});
+            }
         }
     }
 }
