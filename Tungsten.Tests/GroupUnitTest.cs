e5e3325c444a0fba9e4166d623090b0e24d7f3e7
KlasClaywood <klas.claywood@gmail.com>
2016-12-21 11:54:11 +0100

    Group testing and documentation

--- Tungsten.Tests/GroupUnitTest.cs
+++ Tungsten.Tests/GroupUnitTest.cs
@@ -12,6 +12,39 @@ namespace Tungsten.Tests
         {
             Group grupp = new Group();
 
+            string[] GuidParts = grupp.Id.Split('-');
+            int partOneExpected = 8;
+            int partOneActual = GuidParts[0].Length;
+            int partTwoExpected = 4;
+            int partTwoActual = GuidParts[1].Length;
+            int partThreeExpected = 4;
+            int partThreeActual = GuidParts[2].Length;
+            int partFourExpected = 4;
+            int partFourActual = GuidParts[3].Length;
+            int partFiveExpected = 12;
+            int partFiveActual = GuidParts[4].Length;
+
+            Assert.AreEqual(partOneExpected, 
+                            partOneActual);
+            Assert.AreEqual(partTwoExpected, 
+                            partTwoActual);
+            Assert.AreEqual(partThreeExpected, 
+                            partThreeActual);
+            Assert.AreEqual(partFourExpected, 
+                            partFourActual);
+            Assert.AreEqual(partFiveExpected, 
+                            partFiveActual);
+        }
+
+        [TestMethod]
+        public void Group_Id_Set_To_Klas_Returns_Klas()
+        {
+            Group grupp = new Group();
+            string expected = "Klas";
+            grupp.Id = expected;
+            string actual = grupp.Id;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
