05fee1164206115dd7f7c0f457a60fd0984a8279
Emil Eklund <emileklund@outlook.com>
2016-12-27 14:00:08 +0100

    Added Basic Schedule back-end and tidying up in models.

---
+++ Tungsten/Models/ScheduleSegment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tungsten.Models
+{
+    /// <summary>
+    /// Viewmodel for client-side schedule rendering
+    /// </summary>
+    public class ScheduleSegment
+    {
+        public string CourseName { get; set; }
+        public DayOfWeek Day { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Classroom { get; set; }
+    }
+}
