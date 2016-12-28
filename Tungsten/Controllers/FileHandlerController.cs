366a5a9791aea8bfcb6960a56447e2b06006dea9
David-Lundmark <david.lundmarkbredband.net>
2016-12-28 11:15:23 +0100

    For merge

---
+++ Tungsten/Controllers/FileHandlerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Tungsten.DataAccessLayer;
+using Tungsten.Repositories;
+
+namespace Tungsten.Controllers
+{
+    public class FileHandlerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //
+        // GET: /FileHandler/
+        public ActionResult Index()
+        {
+            var files = db.FilePaths;
+            return View(files);
+        }
+    }
+}
