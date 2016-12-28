fe1d0e67bcdb5e5dcb468f4b6e85759c2dab9bf1
MZDisaster <moaazw@gmail.com>
2016-12-28 16:01:31 +0100

    Role specific dashboard home pages.
    Course view.
    Tommy finished learning and he is a teacher now.

---
+++ Tungsten/LMS/pages/dashboard/Pages/home/student/student.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './lms/pages/dashboard/pages/home/student/student.component.html'
+})
+export class StudentHomePage { }
--- Tungsten/LMS/pages/dashboard/dashboard.routes.ts
+++ Tungsten/LMS/pages/dashboard/dashboard.routes.ts
@@ -2,26 +2,44 @@
 import { ModuleWithProviders } from '@angular/core';
 
 //Pages
+// Home Pages
 import { HomePage } from './pages/home/homepage.component';
+import { TeacherHomePage } from './pages/home/teacher/teacher.component';
+import { StudentHomePage } from './pages/home/student/student.component';
+//import { HomePage } from './pages/home/admin/admin.component';
+
+
+//Other Pages
 import { Dashboard_Index } from './dashboard.component';
 import { GroupsPage } from './pages/groups/groups.component';
 import { GroupPage } from './pages/group/group.component';
 import { CreateGroup } from './pages/creategroup/creategroup.component';
 import { CreateCourse } from './pages/createcourse/createcourse.component';
+import { CoursePage } from './pages/course/course.component';
+
 
 // Routing Guards
 import { isAuthenticatedGuard } from '../../services/guards/isAuthenticated';
+import { isTeacherGuard } from '../../services/guards/isTeacher';
+import { isStudentGuard } from '../../services/guards/isStudent';
 
+
+// Note:
+// implement is teacherguard, ( problem adding canActivate on a child )
 const routes: Routes = [
     {
         path: 'dashboard', component: Dashboard_Index, canActivateChild: [isAuthenticatedGuard],
         children:
         [
             { path: '', component: HomePage, outlet: 'dashboard' }, // base url
+            { path: 'student', component: StudentHomePage, outlet: 'dashboard' },
+            { path: 'teacher', component: TeacherHomePage, outlet: 'dashboard' },
+            { path: 'admin', component: HomePage, outlet: 'dashboard' },
             { path: 'groups', component: GroupsPage, outlet: 'dashboard' },
             { path: 'group/:id', component: GroupPage, outlet: 'dashboard' },
             { path: 'creategroup', component: CreateGroup, outlet: 'dashboard' },
-            { path: 'createcourse/:groupid', component: CreateCourse, outlet: 'dashboard' }
+            { path: 'createcourse/:groupid', component: CreateCourse, outlet: 'dashboard' },
+            { path: 'course/:courseid', component: CoursePage, outlet: 'dashboard' }
         ]
     }
 ];
