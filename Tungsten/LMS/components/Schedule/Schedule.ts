b68fa0d063a30bf3d0d91bc177debc722d342fa2
Emil Eklund <emileklund@outlook.com>
2016-12-29 09:29:20 +0100

    Adding a bit more foundation.

--- Tungsten/LMS/components/Schedule/Schedule.ts
+++ Tungsten/LMS/components/Schedule/Schedule.ts
@@ -1,23 +1,30 @@
-﻿import { Component, OnInit, Inject } from '@angular/core';
+﻿import { Component, AfterViewInit, Inject, Input, ViewChild } from '@angular/core';
 import { ScheduleSegment } from '../../interfaces/schedulesegment';
 import { ScheduleService } from '../../services/schedule.service';
 
 
 @Component({
     selector: 'lms-schedule-app',
-    templateUrl: './LMS/components/Schedule/Schedule.html'
+    template: `<canvas id="schedule-canvas"></canvas>`
 })
 
-export class Schedule implements OnInit {
+export class Schedule implements AfterViewInit {
     private scheduleSegments: ScheduleSegment[];
 
-    constructor( @Inject(ScheduleService) private scheduleService: ScheduleService) { }
+    @Input("group-id") groupId: string;
 
-    ngOnInit(): void {
-        this.scheduleService.getSchedule("idgoeshere")
+    constructor( @Inject(ScheduleService) scheduleService: ScheduleService) {
+        scheduleService.getSchedule(this.groupId)
             .subscribe(Segments => {
                 this.scheduleSegments = Segments;
             },
             error => console.error(error));
+    }
+
+    ngAfterViewInit(): void {
+        var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("schedule-canvas");
+        var svg: CanvasRenderingContext2D = canvas.getContext("2d");
+        var height: number = canvas.height;
+        var width: number = canvas.width;
     }
 }
