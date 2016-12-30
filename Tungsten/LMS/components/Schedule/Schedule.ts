925d42652c95453a5582b69dbbd3ed7a690ee29f
Emil Eklund <emileklund@outlook.com>
2016-12-30 15:50:33 +0100

    Completed most of the frame rendering.

--- Tungsten/LMS/components/Schedule/Schedule.ts
+++ Tungsten/LMS/components/Schedule/Schedule.ts
@@ -6,7 +6,7 @@ import { ScheduleService } from '../../services/schedule.service';
 @Component({
     selector: 'lms-schedule-app',
     template:
-    `<h3>Schedule</h3><canvas style="width: 100%" id="schedule-canvas"></canvas>`
+    `<canvas style="width: 100%" id="schedule-canvas">Your browser does not support HTML5 Canvas.</canvas>`
 })
 
 export class Schedule implements AfterViewInit {
@@ -15,7 +15,7 @@ export class Schedule implements AfterViewInit {
     public groupId: string;
 
     constructor( @Inject(ScheduleService) private scheduleService: ScheduleService) { }
-    
+
     ngAfterViewInit(): void {
         console.log("[ScheduleComponent] GroupID Passed: " + this.groupId);
         this.scheduleService.getSchedule(this.groupId)
@@ -24,41 +24,67 @@ export class Schedule implements AfterViewInit {
     }
 
     // DOM Variables
-    private htmlCanvas: HTMLCanvasElement;   
+    private htmlCanvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
 
     // Schedule Size Information
     private height: number;
     private width: number;
 
+    public dayStart: string = "08:00:00";
+    public dayEnd: string = "17:00:00";
 
     drawSchedule(segments: ScheduleSegment[]): void {
-        this.setupCanvas();
 
+        this.setupCanvas();
         this.drawFrame();
 
-        let days: number[] = [1, 2, 3, 4, 5];
-        for (let day of days) {
-            this.renderDay(day, segments.filter((segment) => segment.Day == day));
-        }
+        for (let day = 1; day <= 5; day++)
+            this.renderDay(day - 1, segments.filter((segment) => segment.Day == day));
+
     }
 
     setupCanvas(): void {
+
         // DOM Setup
         this.htmlCanvas = <HTMLCanvasElement>document.getElementById('schedule-canvas');
         this.ctx = this.htmlCanvas.getContext('2d');
 
-        // Style Setup
-        this.htmlCanvas.style.width = '100%';
-        this.htmlCanvas.style.height = '100%';
-
         // Scaling Workaround
-        this.width = this.ctx.canvas.width = this.htmlCanvas.width =  this.htmlCanvas.offsetWidth;
+        this.width = this.ctx.canvas.width = this.htmlCanvas.width = this.htmlCanvas.offsetWidth;
         this.height = this.ctx.canvas.height = this.htmlCanvas.height = this.htmlCanvas.offsetHeight;
+
+        // Fill-White
+        this.ctx.fillStyle = "#ffffff";
+        this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
     drawFrame(): void {
-        
+
+        let rulerWidth: number = 32;
+        this.ctx.strokeRect(0, 0, rulerWidth, this.height);
+
+        let dayLength: number = this.parseTimespan(this.dayEnd) - this.parseTimespan(this.dayStart);
+        console.log("Length of day calculated to be: " + dayLength.toString());
+
+        let hourHeight: number = this.height / dayLength;
+
+        this.ctx.textBaseline = "middle"; 
+        this.ctx.fillStyle = "#000000";
+        this.ctx.font = "8px Arial";
+        this.ctx.beginPath();
+
+        for (let hour = 1; hour <= dayLength; hour++)
+        {
+            this.ctx.moveTo(rulerWidth * 3 / 4, hour * hourHeight);
+            this.ctx.lineTo(rulerWidth, hour * hourHeight);
+            this.ctx.fillText(hour.toString() + ":00", 4, hour * hourHeight)
+        }
+
+        this.ctx.stroke();
+
+        this.ctx.translate(rulerWidth, 0);
+        this.width -= rulerWidth;
     }
 
     renderDay(day: number, segments: ScheduleSegment[]): void {
@@ -69,7 +95,7 @@ export class Schedule implements AfterViewInit {
         this.ctx.save();
 
         let colWidth = this.width / 5;
-        this.ctx.translate(colWidth * (day - 1), 0);
+        this.ctx.translate(colWidth * day, 0);
 
         this.ctx.strokeRect(0, 0, colWidth, this.height);
         segments.forEach((segment): void => {
@@ -77,5 +103,10 @@ export class Schedule implements AfterViewInit {
         });
 
         this.ctx.restore();
+    }
+
+    parseTimespan(timespan: string): number {
+        let parts = timespan.split(':');
+        return Number(parts[0]) + Number(parts[1]) / 60;
     }
 }
