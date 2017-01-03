9be877ea234d2219d7e73c3251e787a33188d617
MZDisaster <moaazw@gmail.com>
2017-01-03 15:45:23 +0100

    Added a forgotten resolver to the commit.

--- Tungsten/LMS/services/resolvers/homepageresolver.ts
+++ Tungsten/LMS/services/resolvers/homepageresolver.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable, Inject } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Http, Response } from '@angular/http';
 import { HomePageModel } from '../../classes/homepagemodel';
 import { Observable } from 'rxjs/Rx';
@@ -9,12 +9,23 @@ export class homepageresolver implements Resolve<HomePageModel> {
 
     constructor(
         @Inject(Http) private http: Http,
+        @Inject(Router) private _Router: Router
     ) {
     }
 
-    resolve(): Observable<HomePageModel> {
-        return this.http.get('/Account/GetHomePage').map((result: Response) => {
-            return <HomePageModel>result.json();
-        })
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<HomePageModel> {
+        let Homepage: Observable<HomePageModel>;
+
+        Homepage = this.http.get('/Account/GetHomePage').map((result: Response) => {
+            return result.json();
+        }).first();
+
+        if (Homepage) {
+            return Homepage;
+        } else {
+            this._Router.navigate(['/']);
+            return null;
+        }
+
     }
 }
